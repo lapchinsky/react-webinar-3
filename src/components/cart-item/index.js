78c0c7bc5c3dc79eb03215da7b85be1d4d26dcba
lapchinsky <lapchinsky0308@gmail.com>
2023-05-21 21:49:49 +0300

    completed task

---
+++ src/components/cart-item/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './style.css';
+
+function Item(props){
+
+  const callbacks = {
+    onClick: () =>  props.onClick(props.item)
+  }
+
+  return (
+    <div className={'Item'}>
+      <div className='Item-code'>{props.item.code}</div>
+      <div className='Item-title'>
+        {props.item.title}
+      </div>
+      <div className='Item-price'>
+        {props.item.price}
+      </div>
+      <div className='Item-count'>
+        {props.item.count}
+      </div>
+      <div className='Item-actions'>
+        <button onClick={callbacks.onClick}>
+          Удалить
+        </button>
+      </div>
+    </div>
+  );
+}
+
+Item.propTypes = {
+  item: PropTypes.shape({
+    code: PropTypes.number,
+    title: PropTypes.string,
+    selected: PropTypes.bool,
+    count: PropTypes.number
+  }).isRequired,
+  onClick: PropTypes.func
+};
+
+Item.defaultProps = {
+  onClick: () => {},
+}
+
+export default React.memo(Item);
